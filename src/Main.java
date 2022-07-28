2363490900ba5925c2b732802002a716cd332e00
klepsla <kleslava3@gmail.com>
2022-07-28 22:41:04 +0300

    Домашнее задание исправлено. Во втором задании на поиск минимального числа я не уверен. Другого решения найти не могу.

--- src/Main.java
+++ src/Main.java
@@ -16,38 +16,28 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
                 spending = spending + arr[i];
         }
-        {
-            System.out.println("Cумма трат за месяц составила " + spending + " рублей.");
-        }
+        System.out.println("Cумма трат за месяц составила " + spending + " рублей.");
+
         //Задание № 2
-        arr = generateRandomArray();
-        int maxSpending = -1;
+        int maxSpending = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > maxSpending)
                 maxSpending = arr[i];
         }
-        {
-            System.out.println("Максимальная сумма трат за месяц составила " + maxSpending + " рублей.");
-        }
-        arr = generateRandomArray();
-        int minSpending = 200000;
-        for (int i = 0; i < arr.length; i++) {
+        System.out.println("Максимальная сумма трат за месяц составила " + maxSpending + " рублей.");
+        int minSpending = arr[0];
+        for (int i = 1; i < arr.length; i++) {
             if (arr[i] < minSpending)
                 minSpending = arr[i];
         }
-            {
-                System.out.println("Минимальная сумма трат за месяц составила " + minSpending + " рублей.");
-            }
+        System.out.println("Минимальная сумма трат за месяц составила " + minSpending + " рублей.");
         //Задание № 3
-        arr = generateRandomArray();
         double middleSpending = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > middleSpending)
-                middleSpending = spending / 30;
+                middleSpending = spending / arr.length;
         }
-            {
-                System.out.println("Средняя сумма трат за месяц составила " + middleSpending + " рублей.");
-            }
+        System.out.println("Средняя сумма трат за месяц составила " + middleSpending + " рублей.");
         //Задание № 4
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
